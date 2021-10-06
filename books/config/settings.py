ab4fc0938e1378e2852b4e20d0852d0dd3861467
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-10-06 16:41:18 +0330

    Added custom user model and it's tests, ended chapter 3.

--- books/config/settings.py
+++ books/config/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My apps
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
