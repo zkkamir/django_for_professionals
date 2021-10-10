35a4b1b32a51c89e711c472e6e4edd854ff7719a
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-10-10 22:15:39 +0330

    Added the pages app with minimum functionality and it's tests, end of chapter 4.

--- books/config/settings.py
+++ books/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # My apps
     "accounts",
+    "pages",
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
